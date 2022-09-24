7fdd4d46cdd27f82c03e1d22d038488b2b41a072
Aguilar-Alejandra <105871266+Aguilar-Alejandra@users.noreply.github.com>
2022-09-24 12:09:43 -0600

    registrar_gestion_estudiantes
    Agregando permisos de usuario

--- clases/permisos_usuario.php
+++ clases/permisos_usuario.php
@@ -19,6 +19,8 @@ if (!isset($_SESSION['id_usuario'])) {
 require_once('../Modelos/permisos_modelo.php');
 
 /*Menu laterales*/
+$_SESSION['gestion_estudiantes_vista'] = 'none'; //24-09-22 Gestión Estudiantes
+$_SESSION['estudiantes_vista'] = 'none'; //24-9-22 Mantenimiento Estudiantes
 $_SESSION['pregunta_vista'] = 'none';
 $_SESSION['usuarios_vista'] = 'none';
 $_SESSION['roles_vista'] = 'none';
@@ -132,6 +134,19 @@ function  permisos_a_roles_visualizar($pantalla, $confirmacion)
       }
    }
    //objetos creacion y gestion
+   if ($_SESSION['pantalla'] == '14027') {
+      if ($_SESSION['confirmacion'] == 'block') {
+         $_SESSION['estudiantes_vista'] = "block";//Registro Estudiantes 24-09-22 Victoria Aguilar
+      }
+   }
+
+   
+   if ($_SESSION['pantalla'] == '14028') {
+      if ($_SESSION['confirmacion'] == 'block') {
+         $_SESSION['gestion_estudiantes_vista'] = "block";//Gestión Estudiantes 24-09-22 Victoria Aguilar
+      }
+   }
+
    if ($_SESSION['pantalla'] == '283' or $_SESSION['pantalla'] == '284') {
       if ($_SESSION['confirmacion'] == 'block') {
          $_SESSION['objetos_vista'] = "block";
