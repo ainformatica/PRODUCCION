502b7ac9fb60a50d3211c62021c37a0fd9c340a5
Aguilar-Alejandra <105871266+Aguilar-Alejandra@users.noreply.github.com>
2022-09-24 14:25:35 -0600

    Update subirperfil_estudiantes.php

--- Controlador/subirperfil_estudiantes.php
+++ Controlador/subirperfil_estudiantes.php
@@ -5,8 +5,8 @@ $MP = new modelo_registro_estudiantes();
 if(is_array($_FILES) && count($_FILES)>0){
 
     if(move_uploaded_file($_FILES["f"]["tmp_name"],"../Imagenes_Perfil_Estudiantes/".$_FILES["f"]["name"])){
-      $nombrearchivo2= '../Imagenes_Perfil_Estudiantes/'.$_FILES["f"]["name"];
-      $consulta=$MP-> Registrar_foto($nombrearchivo2);  
+      $nombrearchivo= '../Imagenes_Perfil_Estudiantes/'.$_FILES["f"]["name"];
+      $consulta=$MP-> Registrar_foto($nombrearchivo);  
       echo $consulta;
     }else{
         echo 0;
