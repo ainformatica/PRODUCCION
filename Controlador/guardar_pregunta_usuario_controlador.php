5215c80e4ce8c7dd7aeddb38ffcdb116a6a10d75
Aguilar-Alejandra <105871266+Aguilar-Alejandra@users.noreply.github.com>
2023-05-23 10:21:12 -0600

    Validación para usuario estudiante
    Validación para que solo los estudiantes completen el formulario auto registro

--- Controlador/guardar_pregunta_usuario_controlador.php
+++ Controlador/guardar_pregunta_usuario_controlador.php
@@ -6,6 +6,9 @@
 
 	require_once ('../clases/Conexion.php');
 
+	$id_persona= $_SESSION['id_persona'];
+	$id_usuario= $_SESSION['id_usuario'];
+	
 $Respuesta=strtoupper ($_POST['txt_respuestapu']);
 $Pregunta=$_POST['combopregunta'];
 
@@ -65,13 +68,32 @@ $resultado_pregunta = $mysqli->query($sql_preguntas);
 
             		 		 }	
 
-					 if ($Contador==$row_parametro_pregunta['valor']) 
+					 if ($Contador==$row_parametro_pregunta['valor']) {
 
-													{
-									/*	$sql_actualizar_estatus = "UPDATE usuarios SET   Estatus=1 WHERE id_usuario= ".$_SESSION[id_usuario]." ";
-												$resultado_actualizar_estatus= $mysqli->query($sql_actualizar_estatus);*/
-
-						    					 header('location: ../vistas/cambiar_clave_x_usuario_vista.php?estatus='.$_SESSION["estatus"].' ');
+						//validación de usuario
+						$sql_verificar_usuario= "select Id_rol from tbl_usuarios where Id_usuario=".$id_usuario." ";
+						$resultado_usuario = $mysqli->query($sql_verificar_usuario);
+						$resultado_us = mysqli_fetch_array($resultado_usuario);
+						$rol=$resultado_us['Id_rol']; 
+						//echo $rol;
+								if ($rol==49){
+								   $sql_actualizar_estatus = "UPDATE tbl_usuarios SET   estado=1 WHERE id_usuario= ".$id_usuario." ";
+								   $resultado_actualizar_estatus= $mysqli->query($sql_actualizar_estatus);
+								   
+								   if ($resultado_actualizar_estatus=true){ 
+												   $sql_actualizar_estado = "UPDATE tbl_personas SET Estado='ACTIVO' WHERE id_persona= $id_persona";
+												   $resultado_actualizar_estado= $mysqli->query($sql_actualizar_estado);
+   
+													header('location: ../vistas/reg_estudiantes_login_vista.php?estatus='.$_SESSION["estatus"].' ');
+													   }
+												   }
+															   else{
+																$sql_actualizar_estatus = "UPDATE tbl_usuarios SET   estado=1 WHERE id_usuario= ".$id_usuario." ";
+					 											$resultado_actualizar_estatus= $mysqli->query($sql_actualizar_estatus);
+																header('location: ../vistas/cambiar_clave_x_usuario_vista.php?estatus=' . $_SESSION["estatus"] . ' ');
+																					   }
+								   
+								   
 													}
 
 											
